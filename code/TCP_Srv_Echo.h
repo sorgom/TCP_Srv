624a1bf06e669bd923d6ddd6616b6c0951d9a6b3
sorgom <110917257+sorgom@users.noreply.github.com>
2024-04-01 01:02:40 +0200

    added comments

--- code/TCP_Srv_Echo.h
+++ code/TCP_Srv_Echo.h
@@ -1,22 +1,22 @@
+//  ============================================================
+//  sample TCP echo server class
+//  ============================================================
+//  created by Manfred Sorgo
 
 #pragma once
 #ifndef TCP_SRV_ECHO_H 
 #define TCP_SRV_ECHO_H
 
 #include "TCP_Srv_Base.h"
-#include <mutex>
 
 class TCP_Srv_Echo : public TCP_Srv_Base
 {
 public:
     inline TCP_Srv_Echo() = default;
-protected:   
+protected:
+    //  simple implementation of process: echo received data
+    //  trace received data if TRACE_ON is defined
     void process(const SOCKET clientSocket, Buffer buff, size_t size, UINT32 nr) final;
-private:
-    //  prevent from parallel output
-    std::mutex mMtx;
-
-    
 };
 
 #endif // _H
